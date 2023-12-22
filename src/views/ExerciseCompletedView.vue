ec33acafb2d6dcf8c536c974a1c795c3e6179e84
kolaj <thekolaj@gmail.com>
2023-12-22 15:03:14 +0200

    feat: added styling

--- src/views/ExerciseCompletedView.vue
+++ src/views/ExerciseCompletedView.vue
@@ -5,7 +5,25 @@ import { RouterLink } from 'vue-router'
 
 <template>
   <main>
-    <h1>Congratulations, you have completed {{ $route.params.exercise }}</h1>
-    <RouterLink :to="{ name: 'home' }" v-focus>Back Home</RouterLink>
+    <h1>Congratulations!</h1>
+    <h2>
+      You have completed '<span>{{ $route.params.exercise }}</span
+      >'
+    </h2>
+    <RouterLink :to="{ name: 'home' }" v-focus>Back to Title</RouterLink>
   </main>
 </template>
+
+<style scoped>
+h1 {
+  color: var(--primary-highlight);
+}
+
+span {
+  color: var(--primary-good);
+}
+
+a {
+  margin: 2rem;
+}
+</style>
