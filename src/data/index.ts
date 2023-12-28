a19987d065fc5e9318b9d778c7ff7f11543229d7
kolaj <thekolaj@gmail.com>
2023-12-28 17:53:42 +0200

    feat: content update!

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,7 +2,7 @@ import easy from './categoryEasy.json'
 import medium from './categoryMedium.json'
 import difficult from './categoryDifficult.json'
 import misspelled from './categoryMisspelled.json'
-import test from './categoryTest.json'
+// import test from './categoryTest.json'
 import type { Data } from './dataUtils'
 
 export default <Data>{
@@ -10,5 +10,4 @@ export default <Data>{
   medium,
   difficult,
   misspelled,
-  test,
 }
